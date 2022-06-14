7a854cfb5212bb906398cbb7a8183e2deae75541
Kaname_Endo <uchiyama.uchiyama.travail@gmail.com>
2022-06-14 16:26:54 +0900

    セキュリティパスワード作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :email, uniqueness: true
   before_validation { email.downcase! }
+  has_secure_password
+  validates :password, length: { minimum: 6 }
 end
