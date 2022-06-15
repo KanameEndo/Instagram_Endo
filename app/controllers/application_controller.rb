157ccfb213dfa96439a3342af947297e65c281e7
Kaname_Endo <uchiyama.uchiyama.travail@gmail.com>
2022-06-16 01:13:07 +0900

    他人の投稿編集できないOK

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
+
   protect_from_forgery with: :exception
   include SessionsHelper
   before_action :login_required
@@ -7,9 +7,5 @@ class ApplicationController < ActionController::Base
   private
   def login_required
   redirect_to new_session_path unless current_user
-  end
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :avatar])
   end
 end
