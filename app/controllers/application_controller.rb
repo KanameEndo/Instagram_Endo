1112311e79af2863cbaae356bf9e5980115fafac
Kaname_Endo <uchiyama.uchiyama.travail@gmail.com>
2022-06-16 00:44:00 +0900

    UserプロフィールでエラーでSTOP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
   include SessionsHelper
   before_action :login_required
@@ -6,5 +7,9 @@ class ApplicationController < ActionController::Base
   private
   def login_required
   redirect_to new_session_path unless current_user
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :avatar])
   end
 end
