80e07346f4a201bf4b72e4c26c5267096644e2a1
Kaname_Endo <uchiyama.uchiyama.travail@gmail.com>
2022-06-16 13:12:47 +0900

    画像表示エラー解消

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,9 @@
 class FavoritesController < ApplicationController
+  def new
+    favorite = current_user.favorite.blog.user.image(blog_id: params[:blog_id])
+    redirect_to user_path, notice: "#{favorite.blog.user.name}さんの記事をお気に入り登録しましたぜ"
+  end
+
   def create
     favorite = current_user.favorites.create(blog_id: params[:blog_id])
     redirect_to blogs_path, notice: "#{favorite.blog.user.name}さんの記事をお気に入り登録しましたぜ"
